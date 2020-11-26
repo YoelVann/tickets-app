8a2cf2c6716aa5cb43c65eeb52e291634462baa1
YoelVann <yoelvann@gmail.com>
2020-11-26 09:03:53 -0600

    first commit

---
+++ src/app/components/chartticketanual/chartticketanual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chartticketanual',
+  templateUrl: './chartticketanual.component.html',
+  styleUrls: ['./chartticketanual.component.css']
+})
+export class ChartticketanualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/chartticketmensual/chartticketmensual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chartticketmensual',
+  templateUrl: './chartticketmensual.component.html',
+  styleUrls: ['./chartticketmensual.component.css']
+})
+export class ChartticketmensualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
